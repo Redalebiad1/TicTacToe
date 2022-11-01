58d5e0300a68873078fd6ba8eb64dd56f1c62e2e
Redalebiad1 <Lebiad.contact@gmail.com>
2022-11-01 20:24:48 +0100

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+const playerConfigOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
+
+const editPLayer1BtnElement = document.getElementById('edit-player-1-btn');
+const editPLayer2BtnElement = document.getElementById('edit-player-2-btn');
+const CancelConfigBtnElement = document.getElementById('cancel-config-btn');
+
+editPLayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPLayer2BtnElement.addEventListener('click', openPlayerConfig);
+CancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
