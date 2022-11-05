6788fcf201d94d752edde86b2ada2f8fd8362a97
Redalebiad1 <Lebiad.contact@gmail.com>
2022-11-05 02:27:07 +0100

    Initial commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,31 @@
+function resetGameStatus(){
+    activePlayer = 0;
+    currentRound = 1;
+    gameIsOver = false;
+    gameAreaElement.firstElementChild.innerHTML = 
+    '<h2>You won, <span id="winner-name">PLAYER NAME</span>!</h2>';
+    gameOverElement.style.display ='none';
+
+    let gameBoardIndex = 0;
+    for(let i = 0; i <3; i++){
+        for(let j = 0; j < 3; j++){
+            gameData[i][j] = 0;
+            gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+            gameBoardItemElement.textContent ='';
+            gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++;
+        }
+    }
+}
+
 function startNewGame(){
     if(players[0].name === '' || players[1].name === '') {
         alert('Please set custom player names for both players!')
         return;
     }
+
+    resetGameStatus();
+
     activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display ='block';
     
@@ -19,7 +42,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event){
-    if (event.target.tagName !== 'LI'){
+    if (event.target.tagName !== 'LI' || gameIsOver){
         return;
     }
 
@@ -37,7 +60,69 @@ function selectGameField(event){
 
 
     gameData[selectedRow][selectedColumn] = activePlayer +1;
-    console.log(gameData);
+    
+    const winnerId = checkForGameOver();
 
+    if(winnerId !== 0){
+        endGame(winnerId);
+    }
+
+    currentRound++;
     switchPlayer();
+}
+
+function checkForGameOver(){
+    // Cheking the rows for equality
+    for (let i = 0; i < 3; i++){
+      if(
+        gameData[i][0] > 0 &&
+        gameData[i][0] === gameData[i][1]  &&
+        gameData[i][1] === gameData[i][2]
+      ) {
+        return gameData[i][0];
+        }  
+    }
+    // Cheking the columns for equality
+    for (let i = 0; i < 3; i++){
+        if(
+          gameData[0][i] > 0 &&
+          gameData[0][i] === gameData[1][i]  &&
+          gameData[0][i] === gameData[2][i]
+        ) {
+          return gameData[0][i];
+        }  
+    }
+    // Diagonal: Top left to bottom right
+    if(
+        gameData[0][0] > 0 &&
+        gameData[0][0] === gameData[1][1]  &&
+        gameData[1][1] === gameData[2][2]
+    ) {
+        return gameData[0][0];
+    }
+    // Diagonal: bottom left to top right
+    if(
+        gameData[2][0] > 0 &&
+        gameData[2][0] === gameData[1][1]  &&
+        gameData[1][1] === gameData[2][2]
+    ) {
+        return gameData[2][0];
+    }
+    if(currentRound === 9){
+        return -1;
+    }
+    return 0;
+      
+}
+function endGame(winnerId){
+    gameIsOver = true;
+    gameOverElement.style.display = 'block';
+
+    if(winnerId > 0 ){
+    const winnerName = players[winnerId -1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+     winnerName;
+    } else{
+        gameOverElement.firstElementChild.textContent ='it\'s a draw!'
+    }
 }
